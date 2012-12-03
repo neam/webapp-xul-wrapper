34c4009e0fd5cba2712dd283825a95820879e7e6
Fredrik Wollsén <fredrik@neam.se>
2012-12-03 03:46:35 +0100

    Tweaked generatesnippet.py for general usage and creating key/value-snippets as demonstrated on https://wiki.mozilla.org/UpdateGeneration

--- update-packaging/generatesnippet.py
+++ update-packaging/generatesnippet.py
@@ -7,7 +7,7 @@ This script generates the complete snippet for a given locale or en-US
 Most of the parameters received are to generate the MAR's download URL
 and determine the MAR's filename
 """
-import sys, os, platform, sha
+import sys, os, platform, hashlib
 from optparse import OptionParser
 from ConfigParser import ConfigParser
 from stat import ST_SIZE
@@ -24,11 +24,6 @@ def main():
                       action="store",
                       dest="applicationIniFile",
                       help="[Required] Specify the absolute path to the application.ini file.")
-    parser.add_option("-l",
-                      "--locale",
-                      action="store",
-                      dest="locale",
-                      help="[Required] Specify which locale we are generating the snippet for.")
     parser.add_option("-p",
                       "--product",
                       action="store",
@@ -38,10 +33,10 @@ def main():
                       action="store",
                       dest="platform",
                       help="[Required] This option is used to indicate which target platform.")
-    parser.add_option("--branch",
+    parser.add_option("--channel",
                       action="store",
-                      dest="branch",
-                      help="This option is used to indicate which branch name to use for FTP file names.")
+                      dest="channel",
+                      help="This option is used to indicate which update channel is used.")
     parser.add_option("--download-base-URL",
                       action="store",
                       dest="downloadBaseURL",
@@ -55,35 +50,40 @@ def main():
     (options, args) = parser.parse_args()
     for req, msg in (('marPath', "the absolute path to the where the MAR file is"),
                      ('applicationIniFile', "the absolute path to the application.ini file."),
-                     ('locale', "a locale."),
                      ('product', "specify a product."),
                      ('platform', "specify the platform.")):
         if not hasattr(options, req):
             parser.error('You must specify %s' % msg)
 
-    if not options.downloadBaseURL or options.downloadBaseURL == '':
-        options.downloadBaseURL = 'http://ftp.mozilla.org/pub/mozilla.org/%s/nightly' % options.product
+    if not options.channel or options.channel == '':
+        options.channel = None
 
-    if not options.branch or options.branch == '':
-        options.branch = None
+    for marType in ('complete', 'partial'):
+        snippet, marFileName = generateSnippet(options.marPath,
+                                  options.applicationIniFile,
+                                  marType,
+                                  options.downloadBaseURL,
+                                  options.product,
+                                  options.platform,
+                                  options.channel)
+        f = open(os.path.join(options.marPath, marFileName+'.update.snippet'), 'wb')
+        f.write(snippet)
+        f.close()
 
-    snippet = generateSnippet(options.marPath,
-                              options.applicationIniFile,
-                              options.locale,
-                              options.downloadBaseURL,
-                              options.product,
-                              options.platform,
-                              options.branch)
-    f = open(os.path.join(options.marPath, 'complete.update.snippet'), 'wb')
-    f.write(snippet)
-    f.close()
+        if options.verbose:
+            # Show in our logs what the contents of the snippet are
+            print snippet
 
-    if options.verbose:
-        # Show in our logs what the contents of the snippet are
-        print snippet
+def getMarFileName(product,appVersion,marType,platform):
+    marFileName = '%s-%s-%s-%s.mar' % (
+        product,
+        appVersion,
+        marType,
+        platform)
+    return marFileName
 
-def generateSnippet(abstDistDir, applicationIniFile, locale,
-                    downloadBaseURL, product, platform, branch):
+def generateSnippet(abstDistDir, applicationIniFile, marType,
+                    downloadBaseURL, product, platform, channel):
     # Let's extract information from application.ini
     c = ConfigParser()
     try:
@@ -92,43 +92,39 @@ def generateSnippet(abstDistDir, applicationIniFile, locale,
        sys.exit(stderror)
     buildid = c.get("App", "BuildID")
     appVersion = c.get("App", "Version")
-    branchName = branch or c.get("App", "SourceRepository").split('/')[-1]
 
-    marFileName = '%s-%s.%s.%s.complete.mar' % (
+    marFileName = getMarFileName(
         product,
         appVersion,
-        locale,
+        marType,
         platform)
     # Let's determine the hash and the size of the MAR file
     # This function exits the script if the file does not exist
     (completeMarHash, completeMarSize) = getFileHashAndSize(
         os.path.join(abstDistDir, marFileName))
     # Construct the URL to where the MAR file will exist
-    interfix = ''
-    if locale == 'en-US':
-        interfix = ''
-    else:
-        interfix = '-l10n'
-    marDownloadURL = "%s/%s%s/%s" % (downloadBaseURL,
-                                     datedDirPath(buildid, branchName),
-                                     interfix,
+    marDownloadURL = "%s/%s/%s/%s" % (downloadBaseURL,
+                                     channel,
+                                     appVersion,
                                      marFileName)
 
-    snippet = """complete
-%(marDownloadURL)s
-sha1
-%(completeMarHash)s
-%(completeMarSize)s
-%(buildid)s
-%(appVersion)s
-%(appVersion)s
-""" % dict( marDownloadURL=marDownloadURL,
+    snippet = """
+type=%(marType)s
+url=%(marDownloadURL)s
+hashFunction=sha512
+hashValue=%(completeMarHash)s
+size=%(completeMarSize)s
+build=%(buildid)s
+appv=%(appVersion)s
+extv=%(appVersion)s
+""" % dict( marType=marType,
+            marDownloadURL=marDownloadURL,
             completeMarHash=completeMarHash,
             completeMarSize=completeMarSize,
             buildid=buildid,
             appVersion=appVersion)
 
-    return snippet
+    return snippet, marFileName
 
 def getFileHashAndSize(filepath):
     sha1Hash = 'UNKNOWN'
@@ -138,29 +134,13 @@ def getFileHashAndSize(filepath):
         # open in binary mode to make sure we get consistent results
         # across all platforms
         f = open(filepath, "rb")
-        shaObj = sha.new(f.read())
+        shaObj = hashlib.sha512(f.read())
         sha1Hash = shaObj.hexdigest()
         size = os.stat(filepath)[ST_SIZE]
     except IOError, (stderror):
        sys.exit(stderror)
 
     return (sha1Hash, size)
-
-def datedDirPath(buildid, milestone):
-    """
-    Returns a string that will look like:
-    2009/12/2009-12-31-09-mozilla-central
-    """
-    year  = buildid[0:4]
-    month = buildid[4:6]
-    day   = buildid[6:8]
-    hour  = buildid[8:10]
-    datedDir = "%s-%s-%s-%s-%s" % (year,
-                                   month,
-                                   day,
-                                   hour,
-                                   milestone)
-    return "%s/%s/%s" % (year, month, datedDir)
 
 if __name__ == '__main__':
     main()
